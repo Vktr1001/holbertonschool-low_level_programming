739f2fd24bb037596ae6448ef7bb691c6cc76d23
Cyberdog1001 <146763600+Vktr1001@users.noreply.github.com>
2025-11-01 21:05:23 -0600

    Update file permissions for 4-puts.c to make it executable

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,3 +1,12 @@
+/**
+ * main - Prints "Programming is like building a multilingual puzzle"
+ *
+ * Description: Uses the puts function from the stdio.h library to print
+ * "Programming is like building a multilingual puzzle" followed by a new line.
+ * The function then returns 0 to indicate successful execution.
+ *
+ * Return: 0 if successful, non-zero otherwise
+ */
 #include <stdio.h>
 
 int main(void)
